ee6726a78a8cc16a5b432a18173316553dc61257
youhh <16621043230@163.com>
2019-01-04 15:03:19 +0800

    同步len3的org包下类

--- tomcat-len4/src/main/java/org/apache/catalina/connector/http10/HttpConnector.java
+++ tomcat-len4/src/main/java/org/apache/catalina/connector/http10/HttpConnector.java
@@ -102,8 +102,7 @@ import org.apache.catalina.util.StringManager;
  */
 
 
-public final class HttpConnector
-    implements Connector, Lifecycle, Runnable {
+public final class HttpConnector implements Connector, Lifecycle, Runnable {
 
 
     // ----------------------------------------------------- Instance Variables
@@ -846,7 +845,7 @@ public final class HttpConnector
             try {
                 ((Lifecycle) processor).start();
             } catch (LifecycleException e) {
-                log("newProcessor", e);
+                log("newProcessor ", e);
                 return (null);
             }
         }
@@ -1046,7 +1045,7 @@ public final class HttpConnector
         try {
             serverSocket = open();
         } catch (IOException ioe) {
-            log("httpConnector, io problem: ", ioe);
+            log("httpConnector, io problem:: ", ioe);
             eRethrow = ioe;
         } catch (KeyStoreException kse) {
             log("httpConnector, keystore problem: ", kse);
